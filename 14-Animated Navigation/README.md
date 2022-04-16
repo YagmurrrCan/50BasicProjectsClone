b11b2cd5784a790e78f024f0a32c40b1a6ccbe74
YagmurrrCan <74114217+YagmurrrCan@users.noreply.github.com>
2022-04-16 04:21:55 +0300

    create Day14

---
+++ 14-Animated Navigation/README.md
@@ -0,0 +1,5 @@
+# #50Days50Projects - Day 14
+
+## Animated Navigation App
+
+![](/14-Animated%20Navigation/ezgif.com-gif-maker.gif)
---
+++ 14-Animated Navigation/app.js
@@ -0,0 +1,7 @@
+
+const nav = document.querySelector("#nav");
+const toggle = document.querySelector("#toggle");
+
+toggle.addEventListener("click", () => {
+    nav.classList.toggle("active")
+});
